081498764521d354c928fe457a47b9d778d243f6
Jeremy Senn <senn.jeremy@gmail.com>
2016-03-21 09:40:33 -0700

    Simplify commodity find by id

--- app/models/commodity.rb
+++ app/models/commodity.rb
@@ -17,12 +17,8 @@ class Commodity
   end
   
   def self.find_by_id(auth_token, yard_id, commodity_id)
-    api_url = "https://71.41.52.58:50002/api/yard/#{yard_id}/commodity?t=100"
-    xml_content = RestClient::Request.execute(method: :get, url: api_url, verify_ssl: false, headers: {:Authorization => "Bearer #{auth_token}"})
-    data= Hash.from_xml(xml_content)
-#    Rails.logger.info data
-    
-    data["ApiPaginatedResponseOfApiCommodity9fKlOoru"]["Items"]["ApiCommodity"].find {|commodity| commodity['Id'] == commodity_id}
+    commodities = Commodity.all(auth_token, yard_id)
+    commodities.find {|commodity| commodity['Id'] == commodity_id}
   end
   
   def self.search(auth_token, yard_id, query_string)
