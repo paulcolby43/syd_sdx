44dd91db01ec02245546e8037f5732daf7ea460f
Jeremy Senn <senn.jeremy@gmail.com>
2016-03-21 09:38:15 -0700

    Don't try to update ticket with customer information if can't find customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -13,15 +13,16 @@ class Customer
     xml_content = RestClient::Request.execute(method: :get, url: api_url, verify_ssl: false, headers: {:Authorization => "Bearer #{auth_token}"})
     data= Hash.from_xml(xml_content)
     
-    data["ApiPaginatedResponseOfApiCustomerC9S9lUui"]["Items"]["ApiCustomer"]
+    if data["ApiPaginatedResponseOfApiCustomerC9S9lUui"]["Items"]["ApiCustomer"].is_a? Hash # Only one result returned, so put it into an array
+      return [data["ApiPaginatedResponseOfApiCustomerC9S9lUui"]["Items"]["ApiCustomer"]]
+    else # Array of results returned
+      return data["ApiPaginatedResponseOfApiCustomerC9S9lUui"]["Items"]["ApiCustomer"]
+    end
   end
   
   def self.find_by_id(auth_token, yard_id, customer_id)
-    api_url = "https://71.41.52.58:50002/api/yard/#{yard_id}/customer"
-    xml_content = RestClient::Request.execute(method: :get, url: api_url, verify_ssl: false, headers: {:Authorization => "Bearer #{auth_token}"})
-    data= Hash.from_xml(xml_content)
-    
-    data["ApiPaginatedResponseOfApiCustomerC9S9lUui"]["Items"]["ApiCustomer"].find {|customer| customer['Id'] == customer_id}
+    customers = Customer.all(auth_token, yard_id)
+    customers.find {|customer| customer['Id'] == customer_id}
   end
   
   def self.search(auth_token, yard_id, query_string)
