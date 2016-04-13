3179dabf0d577829fb9b0cd077b856fbc7bc9bbc
Jeremy Senn <senn.jeremy@gmail.com>
2016-04-13 07:37:56 -0700

    Set the default dragon_api IP and port to what's set in environment variable

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,18 @@
 class Company < ActiveRecord::Base
+  before_save :default_dragon_api
+  
   has_many :users
+  
+  ############################
+  #     Instance Methods     #
+  ############################
+  
+  # Set the default dragon_api IP and port to what's set in environment variable
+  def default_dragon_api
+    self.dragon_api ||= "#{ENV['SCRAP_DRAGON_API_HOST']}:#{ENV['SCRAP_DRAGON_API_PORT']}"
+  end
+  
+  #############################
+  #     Class Methods         #
+  #############################
 end
