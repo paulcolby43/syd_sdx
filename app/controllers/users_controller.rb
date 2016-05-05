535ee3571f27c42598f3aed4d3428572cc875696
Jeremy Senn <senn.jeremy@gmail.com>
2016-05-05 10:42:10 -0700

    Skip ability authorization for confirm_email and :resend_confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
   before_filter :login_required, :except => [:new, :create, :confirm_email, :resend_confirmation_instructions]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-#  load_and_authorize_resource
-#  skip_authorize_resource :only => [:new, :create, :confirm_email, :resend_confirmation_instructions]
+  load_and_authorize_resource  param_method: :user_params, :except => [:new, :create, :confirm_email, :resend_confirmation_instructions]
 
   # GET /users
   # GET /users.json
