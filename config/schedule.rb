2807670d06df508892e3609e2b420324c7f12a8f
Jeremy Senn <senn.jeremy@gmail.com>
2016-05-20 11:07:02 -0700

    Wrong job_type directory in schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -24,7 +24,7 @@ set :environment, 'development'
 
 every :reboot do
   # CentOS VM
-  job_type :application, "cd /usr/local/Ruby/syd_qbo_uat && :task :output"
+  job_type :application, "cd /usr/local/Ruby/syd_sdx_uat && :task :output"
 #  
   # Mac Mini
 #  job_type :application, "cd /Users/syd/RubyProjects/syd_sdx_dev && :task :output"
