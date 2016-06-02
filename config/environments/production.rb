8ddc99f36f1ad19f8c637408b84da10a44fa8f3a
Jeremy Senn <senn.jeremy@gmail.com>
2016-06-02 07:00:21 -0700

    Don't use localhost for mailer config in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 8082 }
+  config.action_mailer.default_url_options = { host: '173.227.154.149', port: 8082 }
   
   config.action_mailer.delivery_method = :smtp
 
