66c6b93d60727ceb4b70a0732b859c4d5c701c25
Jeremy Senn <senn.jeremy@gmail.com>
2016-06-28 07:50:49 -0700

    Updated action mailer host for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: '71.41.52.52' }
+  config.action_mailer.default_url_options = { host: 'sdx.scrapyarddog.com' }
   
   config.action_mailer.delivery_method = :smtp
 
