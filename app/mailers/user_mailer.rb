1893d4e5670cdf970ece556bf21c208d3747ebac
Jeremy Senn <senn.jeremy@gmail.com>
2016-11-16 07:07:57 -0800

    Added adam.greenberg@tranact.com to new user registration email list

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -15,7 +15,7 @@ class UserMailer < ActionMailer::Base
   def new_user_registration(user)
     @user = user
     @to = 'john@tranact.com'
-    @cc = "info@tranact.com, patrick@tranact.com, ken@tranact.com, brian@tranact.com, jeremy@tranact.com, tim@tranact.com, colby@tranact.com"
+    @cc = "info@tranact.com, patrick@tranact.com, ken@tranact.com, brian@tranact.com, jeremy@tranact.com, tim@tranact.com, colby@tranact.com, adam.greenberg@tranact.com"
     mail(to: @to, cc: @cc, subject: 'SYD SDX New User Sign Up')
   end
 
