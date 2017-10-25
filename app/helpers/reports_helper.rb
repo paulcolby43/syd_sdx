505de258d0a51cb6bec18bad5063651e49739992
Jeremy Senn <senn.jeremy@gmail.com>
2017-10-25 06:15:32 -0700

    Remove delimiter from donut graph because it messes things up

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -2,13 +2,13 @@ module ReportsHelper
   
   def ticket_commodity_weight_donut_data(line_items)
     line_items.group_by{ |i| i['PrintDescription'] }.map do |(line_item_group, line_items)|
-      {label: line_item_group, value: number_with_delimiter(line_items.map { |i| i['NetWeight'].to_d }.sum.round) }
+      {label: line_item_group, value: line_items.map { |i| i['NetWeight'].to_d }.sum.round }
     end
   end
   
   def ticket_commodity_amount_donut_data(line_items)
     line_items.group_by{ |i| i['PrintDescription'] }.map do |(line_item_group, line_items)|
-      {label: line_item_group, value: number_with_delimiter(line_items.map { |i| i['ExtendedAmount'].to_d }.sum.round) }
+      {label: line_item_group, value: line_items.map { |i| i['ExtendedAmount'].to_d }.sum.round }
     end
   end
   
