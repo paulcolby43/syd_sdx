ff4a4cc54ea0c669ab6d8f65e5f5f04ad72a0790
Jeremy Senn <senn.jeremy@gmail.com>
2017-12-08 07:53:14 -0800

    Show task type description

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,6 +1,13 @@
 module TasksHelper
+  
   def task_status_string(status_number)
     status_hash = {"0" => "In Queue", "1" => "Started", "2" => "Completed", "3" => "Void", "4" => "Delayed"}
     return status_hash[status_number]
   end
+  
+  def task_type_string(type_number)
+    type_hash = {"0" => "Pickup Container", "1" => "Dropoff Container", "2" => "No Container", "3" => "Dump Container", "4" => "Empty Container", "5" => "Cross State Lines"}
+    return type_hash[type_number]
+  end
+  
 end
