fbbb27da2e4ec7e7f1311f70718f861c6b638a46
Jeremy Senn <senn.jeremy@gmail.com>
2018-01-04 13:11:43 -0800

    Use content_type => 'application/json' when sending task update to dragon

--- app/models/task.rb
+++ app/models/task.rb
@@ -39,18 +39,8 @@ class Task
     json_encoded_payload = JSON.generate(payload)
     Rails.logger.info "Task.update json encoded payload: #{json_encoded_payload}"
     
-    response = RestClient::Request.execute(method: :post, url: api_url, verify_ssl: false, headers: {:Authorization => "Bearer #{auth_token}", :Accept => "application/xml"},
-      payload: {
-        "MobileDispatchTaskInformation" => {
-          "Id" => task[:id],
-          "DispatchTripId" => task[:trip_id],
-          "Notes" => task[:notes],
-          "StartingMileage" => task[:starting_mileage],
-          "EndingMileage" => task[:ending_mileage],
-          "TaskStatus" => task[:status],
-          "IsUpdateRequired" => true
-          }
-        })
+    response = RestClient::Request.execute(method: :post, url: api_url, verify_ssl: false, headers: {:Authorization => "Bearer #{auth_token}", :Accept => "application/xml", :content_type => 'application/json'},
+      payload: payload)
       
       Rails.logger.info "Task update response: #{response}"
       data= Hash.from_xml(response)
