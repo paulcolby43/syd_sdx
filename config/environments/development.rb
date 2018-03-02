502c734296c742909a4d3cea9d7a0d3c3ce67a61
Jeremy Senn <senn.jeremy@gmail.com>
2018-03-02 08:33:24 -0800

    Use environment variable for determining host to use in action mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,8 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 8081 }
+#  config.action_mailer.default_url_options = { host: 'localhost', port: 8081 }
+  config.action_mailer.default_url_options = { host: ENV['SCRAP_YARD_DOG_HOST'] }
   
   config.action_mailer.delivery_method = :smtp
 
