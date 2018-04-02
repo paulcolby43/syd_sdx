ebac624a0cca55a229751a83ee5b0a06de2244bc
Jeremy Senn <senn.jeremy@gmail.com>
2018-04-02 11:13:58 -0700

    Put ssl client read back as was

--- app/models/image.rb
+++ app/models/image.rb
@@ -122,7 +122,7 @@ class Image < ActiveRecord::Base
     ssl_client.sync_close = true
     ssl_client.puts command
     response = ssl_client.sysread(200000) # Read up to 200,000 bytes
-#    ssl_client.close
+    ssl_client.close
     
     Rails.logger.debug "***********Image.api_find_all_by_ticket_number response: #{response}"
     data= Hash.from_xml(response.gsub(/&/, '/&amp;')) # Convert xml response to a hash, escaping ampersands first
