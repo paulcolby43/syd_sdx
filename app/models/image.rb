04b902a38adba6ade40a2912603614787ea037f0
Jeremy Senn <senn.jeremy@gmail.com>
2018-04-04 16:16:07 -0700

    Debugging ticket show

--- app/models/image.rb
+++ app/models/image.rb
@@ -125,7 +125,6 @@ class Image < ActiveRecord::Base
     response = ""
     while line = ssl_client.gets
       response = response + line
-      puts line
       break if (line.to_s.strip == '</RESULT>') or (line.to_s.strip == '<RESULT>EOF</RESULT>') # Last line or no results
     end
     
