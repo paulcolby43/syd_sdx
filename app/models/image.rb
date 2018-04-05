b01b36d1c223d63df8783675b5647d41c89d21c4
Jeremy Senn <senn.jeremy@gmail.com>
2018-04-04 17:07:26 -0700

    Debug images xml

--- app/models/image.rb
+++ app/models/image.rb
@@ -125,6 +125,7 @@ class Image < ActiveRecord::Base
     response = ""
     while line = ssl_client.gets
       response = response + line
+      puts line
 #      break if (line.to_s.strip == '</RESULT>') or (line.to_s.strip == '<RESULT>EOF</RESULT>') # Last line or no results
       break unless (line.start_with?("<ROW>"))
     end
