b94642a778018be6683a83dd80d81d3cd2a195cf
Jeremy <senn.jeremy@gmail.com>
2018-04-17 10:16:32 -0700

    Testing ticket edit page

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -110,30 +110,30 @@ class TicketsController < ApplicationController
   # GET /tickets/1/edit
   def edit
     authorize! :edit, :tickets
-    @drawers = Drawer.all(current_user.token, current_yard_id, current_user.currency_id)
-    @checking_accounts = CheckingAccount.all(current_user.token, current_yard_id)
-    @ticket = Ticket.find_by_id(current_user.token, current_yard_id, params[:id])
-    @accounts_payable_items = AccountsPayable.all(current_user.token, current_yard_id, params[:id])
-    @ticket_number = @ticket["TicketNumber"]
-    @images_array = Image.api_find_all_by_ticket_number(@ticket_number, current_user.company, current_yard_id).reverse # Ticket images
-    @line_items = @ticket["TicketItemCollection"]["ApiTicketItem"].select {|i| i["Status"] == '0'} unless @ticket["TicketItemCollection"].blank?
-    @commodities = Commodity.all(current_user.token, current_yard_id)
-#    @images = Image.where(ticket_nbr: @ticket["TicketNumber"], yardid: current_yard_id)
-#    @contract = Yard.contract(current_yard_id)
-    @apcashier = Apcashier.find_by_id(current_user.token, current_yard_id, @accounts_payable_items.first['CashierId']) if @ticket['Status'] == '3'
-#    AccountsPayable.update(current_user.token, current_yard_id, params[:id], @accounts_payable_items.last)
-    rt_lookups = RtLookup.api_find_all_by_ticket_number(@ticket_number, current_user.company, current_yard_id)
-    rt_lookups.each do |rt_lookup|
-      rt_lookup_images = Image.api_find_all_by_receipt_number(rt_lookup['RECEIPT_NBR'], current_user.company, current_yard_id).reverse
-      @images_array =  @images_array | rt_lookup_images # Union the image arrays
-    end
-    @combolists = Vehicle.combolists(current_user.token)
-    unless @combolists.blank?
-      @vehicle_makes = @combolists["VehicleMakes"]["VehicleMakeInformation"]
-      @vehicle_models = @combolists["VehicleModels"]["VehicleModelInformation"]
-      @body_styles = @combolists["VehicleBodyStyles"]["UserDefinedListValueQuickInformation"]
-      @vehicle_colors = @combolists["VehicleColors"]["UserDefinedListValueQuickInformation"]
-    end
+#    @drawers = Drawer.all(current_user.token, current_yard_id, current_user.currency_id)
+#    @checking_accounts = CheckingAccount.all(current_user.token, current_yard_id)
+#    @ticket = Ticket.find_by_id(current_user.token, current_yard_id, params[:id])
+#    @accounts_payable_items = AccountsPayable.all(current_user.token, current_yard_id, params[:id])
+#    @ticket_number = @ticket["TicketNumber"]
+#    @images_array = Image.api_find_all_by_ticket_number(@ticket_number, current_user.company, current_yard_id).reverse # Ticket images
+#    @line_items = @ticket["TicketItemCollection"]["ApiTicketItem"].select {|i| i["Status"] == '0'} unless @ticket["TicketItemCollection"].blank?
+#    @commodities = Commodity.all(current_user.token, current_yard_id)
+##    @images = Image.where(ticket_nbr: @ticket["TicketNumber"], yardid: current_yard_id)
+##    @contract = Yard.contract(current_yard_id)
+#    @apcashier = Apcashier.find_by_id(current_user.token, current_yard_id, @accounts_payable_items.first['CashierId']) if @ticket['Status'] == '3'
+##    AccountsPayable.update(current_user.token, current_yard_id, params[:id], @accounts_payable_items.last)
+#    rt_lookups = RtLookup.api_find_all_by_ticket_number(@ticket_number, current_user.company, current_yard_id)
+#    rt_lookups.each do |rt_lookup|
+#      rt_lookup_images = Image.api_find_all_by_receipt_number(rt_lookup['RECEIPT_NBR'], current_user.company, current_yard_id).reverse
+#      @images_array =  @images_array | rt_lookup_images # Union the image arrays
+#    end
+#    @combolists = Vehicle.combolists(current_user.token)
+#    unless @combolists.blank?
+#      @vehicle_makes = @combolists["VehicleMakes"]["VehicleMakeInformation"]
+#      @vehicle_models = @combolists["VehicleModels"]["VehicleModelInformation"]
+#      @body_styles = @combolists["VehicleBodyStyles"]["UserDefinedListValueQuickInformation"]
+#      @vehicle_colors = @combolists["VehicleColors"]["UserDefinedListValueQuickInformation"]
+#    end
   end
 
   # PATCH/PUT /tickets/1
