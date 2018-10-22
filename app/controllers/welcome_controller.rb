f0d4799fb810d09e45e7457df183d47df7782084
Jeremy <senn.jeremy@gmail.com>
2018-10-22 10:44:06 -0700

    First pieces of KPI Dashboard for admin users

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -11,16 +11,22 @@ class WelcomeController < ApplicationController
   end
   
   def kpi_dashboard
-    @yards = Yard.all(current_user.token)
-    unless params[:yard_id].blank?
-      @yard = Yard.find_by_id(current_user.token, params[:yard_id])
-      session[:yard_id] = params[:yard_id]
-      session[:yard_name] = @yard['Name']
+    if current_user.admin?
+      @yards = Yard.all(current_user.token)
+      unless params[:yard_id].blank?
+        @yard = Yard.find_by_id(current_user.token, params[:yard_id])
+        session[:yard_id] = params[:yard_id]
+        session[:yard_name] = @yard['Name']
+      end
+      @held_tickets_today = Ticket.all_last_90_days(2, current_user.token, current_yard_id)
+      @paid_tickets_today = Ticket.all_last_90_days(3, current_user.token, current_yard_id)
+
+      @held_shipments_today = PackShipment.all_held(current_user.token, current_yard_id).last(15)
+      @closed_shipments_today = PackShipment.all_by_date(current_user.token, current_yard_id, (Date.today - 4.months), Date.today)
+    else
+      flash[:danger] = 'You do not have access to that page.'
+      redirect_to root_path
     end
-    @held_tickets_today = Ticket.all_last_90_days(2, current_user.token, current_yard_id)
-    @paid_tickets_today = Ticket.all_last_90_days(3, current_user.token, current_yard_id)
-    
-    @held_shipments_today = PackShipment.all_held(current_user.token, current_yard_id).last(15)
-    @closed_shipments_today = PackShipment.all_by_date(current_user.token, current_yard_id, (Date.today - 4.months), Date.today)
   end
+  
 end
