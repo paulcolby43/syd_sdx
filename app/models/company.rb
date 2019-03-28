3a1ff90d7f9f32552c6ee866ac62cedab7687592
Jeremy <senn.jeremy@gmail.com>
2019-03-28 12:00:35 -0700

    Don't try and geolocate company if no address information

--- app/models/company.rb
+++ app/models/company.rb
@@ -53,14 +53,20 @@ class Company < ActiveRecord::Base
     end
   end
   
-  def geolocation
-    "https://maps.googleapis.com/maps/api/geocode/json?address=#{CGI.escape(full_address_string)}&key=#{ENV['GOOGLE_MAPS_API_KEY']}"
+  def geolocation_api_url
+    unless full_address_string.blank?
+      "https://maps.googleapis.com/maps/api/geocode/json?address=#{CGI.escape(full_address_string)}&key=#{ENV['GOOGLE_MAPS_API_KEY']}"
+    end
   end
   
   def geolocation_json
-    json_data = RestClient::Request.execute(method: :get, url: geolocation, headers: {:Accept => "application/json"})
-    unless json_data.blank?
-      return JSON.parse(json_data, object_class: OpenStruct)
+    unless geolocation_api_url.blank?
+      json_data = RestClient::Request.execute(method: :get, url: geolocation_api_url, headers: {:Accept => "application/json"})
+      unless json_data.blank?
+        return JSON.parse(json_data, object_class: OpenStruct)
+      else
+        return nil
+      end
     else
       return nil
     end
