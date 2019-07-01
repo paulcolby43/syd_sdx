73b514cfad40698953d3151558db5ceaae6d9767
Jeremy <senn.jeremy@gmail.com>
2019-07-01 12:13:25 -0700

    new scoreboard api calls implemented

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -27,89 +27,107 @@ class WelcomeController < ApplicationController
       @tickets_on_hold_total = @tickets_on_hold['TicketsOnHold'] unless @tickets_on_hold.blank?
       @average_hold_time = @tickets_on_hold['AverageHoldTime'] unless @tickets_on_hold.blank?
       
-      @closed_tickets = Ticket.all_last_30_days(1, current_user.token, current_yard_id)
-      @held_tickets = Ticket.all_last_30_days(2, current_user.token, current_yard_id)
-      @paid_tickets = Ticket.all_last_30_days(3, current_user.token, current_yard_id)
+      @paid_tickets_today_data = Scoreboard.paid_tickets_today_data(current_user.token, current_yard_id)
+      @paid_tickets_today_count = @paid_tickets_today_data['PaidTickets']
+      @paid_tickets_today_amount = @paid_tickets_today_data['PaidTicketsAmount']
+      
+      @commodity_totals_by_type = Scoreboard.commodity_totals_by_type(current_user.token, current_yard_id)
+      
+      @held_shipments_count = Scoreboard.held_shipments_count(current_user.token, current_yard_id)
+      @sent_shipments_count = Scoreboard.sent_shipments_count(current_user.token, current_yard_id)
+      
+      @held_tickets_today = Scoreboard.held_tickets_today(current_user.token, current_yard_id)
+      @closed_tickets_today = Scoreboard.closed_tickets_today(current_user.token, current_yard_id)
+      @paid_tickets_today = Scoreboard.paid_tickets_today(current_user.token, current_yard_id)
+      @closed_and_held_and_paid_tickets_today = @held_tickets_today + @closed_tickets_today + @paid_tickets_today
+      
+      @closed_shipments_today = Scoreboard.closed_shipments_today(current_user.token, current_yard_id)
+#      @held_shipments_today = Scoreboard.held_shipments_today(current_user.token, current_yard_id)
+#      @held_and_closed_shipments_today = @held_shipments_today + @closed_shipments_today
+      
+#      @closed_tickets = Ticket.all_last_30_days(1, current_user.token, current_yard_id)
+#      @held_tickets = Ticket.all_last_30_days(2, current_user.token, current_yard_id)
+#      @paid_tickets = Ticket.all_last_30_days(3, current_user.token, current_yard_id)
       
 #      @closed_tickets = Ticket.all_by_date_and_status_and_yard(1, current_user.token, current_yard_id, 30.days.ago, Date.today.yesterday)
 #      @held_tickets = Ticket.all_by_date_and_status_and_yard(2, current_user.token, current_yard_id, 30.days.ago, Date.today.yesterday)
 #      @paid_tickets = Ticket.all_by_date_and_status_and_yard(3, current_user.token, current_yard_id, 30.days.ago, Date.today.yesterday)
       
-      @closed_and_paid_tickets = @closed_tickets + @paid_tickets
-      @closed_and_held_and_paid_tickets = @closed_tickets + @held_tickets + @paid_tickets
+#      @closed_and_paid_tickets = @closed_tickets + @paid_tickets
+#      @closed_and_held_and_paid_tickets = @closed_tickets + @held_tickets + @paid_tickets
       
-      @closed_tickets_today = Ticket.all_today(1, current_user.token, current_yard_id)
-      @held_tickets_today = Ticket.all_today(2, current_user.token, current_yard_id)
-      @paid_tickets_today = Ticket.all_today(3, current_user.token, current_yard_id)
-      @closed_and_paid_tickets_today = @closed_tickets_today + @paid_tickets_today
+#      @closed_tickets_today = Ticket.all_today(1, current_user.token, current_yard_id)
+#      @held_tickets_today = Ticket.all_today(2, current_user.token, current_yard_id)
+#      @paid_tickets_today = Ticket.all_today(3, current_user.token, current_yard_id)
+#      @closed_and_paid_tickets_today = @closed_tickets_today + @paid_tickets_today
       
-      @wait_time = Ticket.average_wait_time(@closed_and_paid_tickets_today)
+#      @wait_time = Ticket.average_wait_time(@closed_and_paid_tickets_today)
       
-      @held_tickets_today_total = 0
-      @held_tickets_today.each do |ticket|
-        @held_tickets_today_total = @held_tickets_today_total + (ticket['TicketItemCollection'].blank? ? 0 : Ticket.line_items_total(ticket['TicketItemCollection']['ApiTicketItem'])) 
-      end
+#      @held_tickets_today_total = 0
+#      @held_tickets_today.each do |ticket|
+#        @held_tickets_today_total = @held_tickets_today_total + (ticket['TicketItemCollection'].blank? ? 0 : Ticket.line_items_total(ticket['TicketItemCollection']['ApiTicketItem'])) 
+#      end
       
-      @paid_tickets_today_total = 0
-      @paid_tickets_today.each do |ticket|
-        @paid_tickets_today_total = @paid_tickets_today_total + (ticket['TicketItemCollection'].blank? ? 0 : Ticket.line_items_total(ticket['TicketItemCollection']['ApiTicketItem'])) 
-      end
+#      @paid_tickets_today_total = 0
+#      @paid_tickets_today.each do |ticket|
+#        @paid_tickets_today_total = @paid_tickets_today_total + (ticket['TicketItemCollection'].blank? ? 0 : Ticket.line_items_total(ticket['TicketItemCollection']['ApiTicketItem'])) 
+#      end
       
-      @closed_and_paid_tickets_line_items = []
-      unless @closed_and_paid_tickets.blank?
-        @closed_and_paid_tickets.each do |ticket|
-          unless ticket['TicketItemCollection'].blank? or ticket['TicketItemCollection']['ApiTicketItem'].blank?
-            @closed_and_paid_tickets_line_items = @closed_and_paid_tickets_line_items + Ticket.line_items(ticket['TicketItemCollection']['ApiTicketItem'])
-          end
-        end
-      end
+#      @closed_and_paid_tickets_line_items = []
+#      unless @closed_and_paid_tickets.blank?
+#        @closed_and_paid_tickets.each do |ticket|
+#          unless ticket['TicketItemCollection'].blank? or ticket['TicketItemCollection']['ApiTicketItem'].blank?
+#            @closed_and_paid_tickets_line_items = @closed_and_paid_tickets_line_items + Ticket.line_items(ticket['TicketItemCollection']['ApiTicketItem'])
+#          end
+#        end
+#      end
       
-      @closed_and_paid_tickets_line_items_net_total = 0
-      @closed_and_paid_tickets_line_items.each do |line_item|
-        @closed_and_paid_tickets_line_items_net_total = @closed_and_paid_tickets_line_items_net_total + line_item["NetWeight"].to_d
-      end
+#      @closed_and_paid_tickets_line_items_net_total = 0
+#      @closed_and_paid_tickets_line_items.each do |line_item|
+#        @closed_and_paid_tickets_line_items_net_total = @closed_and_paid_tickets_line_items_net_total + line_item["NetWeight"].to_d
+#      end
       
-      @closed_and_paid_tickets_today_line_items = []
-      unless @closed_and_paid_tickets_today.blank?
-        @closed_and_paid_tickets_today.each do |ticket|
-          unless ticket['TicketItemCollection'].blank? or ticket['TicketItemCollection']['ApiTicketItem'].blank?
-            if ticket['DateCreated'].to_date == Date.today
-              @closed_and_paid_tickets_today_line_items = @closed_and_paid_tickets_today_line_items + Ticket.line_items(ticket['TicketItemCollection']['ApiTicketItem'])
-            end
-          end
-        end
-      end
+#      @closed_and_paid_tickets_today_line_items = []
+#      unless @closed_and_paid_tickets_today.blank?
+#        @closed_and_paid_tickets_today.each do |ticket|
+#          unless ticket['TicketItemCollection'].blank? or ticket['TicketItemCollection']['ApiTicketItem'].blank?
+#            if ticket['DateCreated'].to_date == Date.today
+#              @closed_and_paid_tickets_today_line_items = @closed_and_paid_tickets_today_line_items + Ticket.line_items(ticket['TicketItemCollection']['ApiTicketItem'])
+#            end
+#          end
+#        end
+#      end
       
-      @closed_and_paid_tickets_today_line_items_ferrous_net_total = 0
-      @closed_and_paid_tickets_today_line_items_non_ferrous_net_total = 0
-      @closed_and_paid_tickets_today_line_items.each do |line_item|
-        commodity = Commodity.find_by_id(current_user.token, current_yard_id, line_item["CommodityId"])
-        if commodity and commodity["Type"] == "F"
-          @closed_and_paid_tickets_today_line_items_ferrous_net_total = @closed_and_paid_tickets_today_line_items_ferrous_net_total + line_item["NetWeight"].to_d
-        elsif commodity and commodity["Type"] == "N"
-          @closed_and_paid_tickets_today_line_items_non_ferrous_net_total = @closed_and_paid_tickets_today_line_items_non_ferrous_net_total + line_item["NetWeight"].to_d
-        end
-      end
+#      @closed_and_paid_tickets_today_line_items_ferrous_net_total = 0
+#      @closed_and_paid_tickets_today_line_items_non_ferrous_net_total = 0
+#      @closed_and_paid_tickets_today_line_items.each do |line_item|
+#        commodity = Commodity.find_by_id(current_user.token, current_yard_id, line_item["CommodityId"])
+#        if commodity and commodity["Type"] == "F"
+#          @closed_and_paid_tickets_today_line_items_ferrous_net_total = @closed_and_paid_tickets_today_line_items_ferrous_net_total + line_item["NetWeight"].to_d
+#        elsif commodity and commodity["Type"] == "N"
+#          @closed_and_paid_tickets_today_line_items_non_ferrous_net_total = @closed_and_paid_tickets_today_line_items_non_ferrous_net_total + line_item["NetWeight"].to_d
+#        end
+#      end
       
-      @closed_tickets_line_items = []
-      unless @closed_tickets.blank?
-        @closed_tickets.each do |ticket|
-          unless ticket['TicketItemCollection'].blank? or ticket['TicketItemCollection']['ApiTicketItem'].blank?
-            @closed_tickets_line_items = @closed_tickets_line_items + Ticket.line_items(ticket['TicketItemCollection']['ApiTicketItem'])
-          end
-        end
-      end
+#      @closed_tickets_line_items = []
+#      unless @closed_tickets.blank?
+#        @closed_tickets.each do |ticket|
+#          unless ticket['TicketItemCollection'].blank? or ticket['TicketItemCollection']['ApiTicketItem'].blank?
+#            @closed_tickets_line_items = @closed_tickets_line_items + Ticket.line_items(ticket['TicketItemCollection']['ApiTicketItem'])
+#          end
+#        end
+#      end
       
-      @closed_tickets_line_items_ferrous_net_total = 0
-      @closed_tickets_line_items_non_ferrous_net_total = 0
-      @closed_tickets_line_items.each do |line_item|
-        commodity = Commodity.find_by_id(current_user.token, current_yard_id, line_item["CommodityId"])
-        if commodity and commodity["Type"] == "F"
-          @closed_tickets_line_items_ferrous_net_total = @closed_tickets_line_items_ferrous_net_total + line_item["NetWeight"].to_d
-        elsif commodity and commodity["Type"] == "N"
-          @closed_tickets_line_items_non_ferrous_net_total = @closed_tickets_line_items_non_ferrous_net_total + line_item["NetWeight"].to_d
-        end
-      end
+#      @closed_tickets_line_items_ferrous_net_total = 0
+#      @closed_tickets_line_items_non_ferrous_net_total = 0
+#      @closed_tickets_line_items.each do |line_item|
+#        commodity = Commodity.find_by_id(current_user.token, current_yard_id, line_item["CommodityId"])
+#        if commodity and commodity["Type"] == "F"
+#          @closed_tickets_line_items_ferrous_net_total = @closed_tickets_line_items_ferrous_net_total + line_item["NetWeight"].to_d
+#        elsif commodity and commodity["Type"] == "N"
+#          @closed_tickets_line_items_non_ferrous_net_total = @closed_tickets_line_items_non_ferrous_net_total + line_item["NetWeight"].to_d
+#        end
+#      end
       
 #      @closed_tickets_today_line_items = []
 #      unless @closed_tickets_today.blank?
@@ -125,30 +143,30 @@ class WelcomeController < ApplicationController
 #        @closed_tickets_today_line_items_net_total = @closed_tickets_today_line_items_net_total + line_item["NetWeight"].to_d
 #      end
       
-      @closed_and_held_and_paid_tickets_today = @closed_tickets_today + @held_tickets_today + @paid_tickets_today
-      @closed_and_held_and_paid_tickets_today = @closed_and_held_and_paid_tickets_today.select {|t| t['DateCreated'].to_date == Date.today}.sort_by{|t| t['DateCreated']}.reverse
+#      @closed_and_held_and_paid_tickets_today = @closed_tickets_today + @held_tickets_today + @paid_tickets_today
+#      @closed_and_held_and_paid_tickets_today = @closed_and_held_and_paid_tickets_today.select {|t| t['DateCreated'].to_date == Date.today}.sort_by{|t| t['DateCreated']}.reverse
 
 #      @held_shipments_today = PackShipment.all_held(current_user.token, current_yard_id).last(5)
 #      @closed_shipments_today = PackShipment.all_by_date(current_user.token, current_yard_id, Date.today, Date.today)
       
-      @held_shipments_today = PackShipment.all_held_today(current_user.token, current_yard_id)
-      @last_held_shipment_today = @held_shipments_today.sort_by{|s| s['DateCreated']}.last unless @held_shipments_today.blank?
+#      @held_shipments_today = PackShipment.all_held_today(current_user.token, current_yard_id)
+#      @last_held_shipment_today = @held_shipments_today.sort_by{|s| s['DateCreated']}.last unless @held_shipments_today.blank?
       
-      @closed_shipments_today = PackShipment.all_closed_today(current_user.token, current_yard_id)
-      @last_closed_shipment_today = @closed_shipments_today.sort_by{|s| s['DateShipped']}.last unless @closed_shipments_today.blank?
+#      @closed_shipments_today = PackShipment.all_closed_today(current_user.token, current_yard_id)
+#      @last_closed_shipment_today = @closed_shipments_today.sort_by{|s| s['DateShipped']}.last unless @closed_shipments_today.blank?
       
-      @closed_and_held_shipments_today = @held_shipments_today + @closed_shipments_today
-      @closed_and_held_shipments_today = @closed_and_held_shipments_today.select {|t| t['DateCreated'].to_date == Date.today}.sort_by{|t| t['DateCreated']}.reverse
+#      @closed_and_held_shipments_today = @held_shipments_today + @closed_shipments_today
+#      @closed_and_held_shipments_today = @closed_and_held_shipments_today.select {|t| t['DateCreated'].to_date == Date.today}.sort_by{|t| t['DateCreated']}.reverse
       
-      @held_shipments_today_total_net = 0
-      @held_shipments_today.each do |shipment|
-        @held_shipments_today_total_net = @held_shipments_today_total_net + shipment['NetWeight'].to_d
-      end
+#      @held_shipments_today_total_net = 0
+#      @held_shipments_today.each do |shipment|
+#        @held_shipments_today_total_net = @held_shipments_today_total_net + shipment['NetWeight'].to_d
+#      end
       
-      @closed_shipments_today_total_net = 0
-      @closed_shipments_today.each do |shipment|
-        @closed_shipments_today_total_net = @closed_shipments_today_total_net + shipment['NetWeight'].to_d
-      end
+#      @closed_shipments_today_total_net = 0
+#      @closed_shipments_today.each do |shipment|
+#        @closed_shipments_today_total_net = @closed_shipments_today_total_net + shipment['NetWeight'].to_d
+#      end
       
 #      @accounts_payable_items = AccountsPayable.all(current_user.token, @paid_tickets_today.last["YardId"], @paid_tickets_today.last['Id'])
 #      @apcashier = Apcashier.find_by_id(current_user.token, @paid_tickets_today.last["YardId"], @accounts_payable_items.first['CashierId'])
--- app/models/scoreboard.rb
+++ app/models/scoreboard.rb
@@ -8,7 +8,7 @@ class Scoreboard
     
     xml_content = RestClient::Request.execute(method: :get, url: api_url, verify_ssl: false, headers: {:Authorization => "Bearer #{auth_token}", :Accept => "application/xml"})
     data= Hash.from_xml(xml_content)
-#    Rails.logger.info data
+#    Rails.logger.info "Scoreboard.tickets_created: #{data}"
     
     return data["GetTicketsCreatedResponse"]
   end
@@ -21,7 +21,7 @@ class Scoreboard
     
     xml_content = RestClient::Request.execute(method: :get, url: api_url, verify_ssl: false, headers: {:Authorization => "Bearer #{auth_token}", :Accept => "application/xml"})
     data= Hash.from_xml(xml_content)
-#    Rails.logger.info data
+#    Rails.logger.info "Scoreboard.tickets_created_number_of_days: #{data}"
     
     return data["GetTicketsCreatedResponse"]
   end
@@ -34,9 +34,185 @@ class Scoreboard
     
     xml_content = RestClient::Request.execute(method: :get, url: api_url, verify_ssl: false, headers: {:Authorization => "Bearer #{auth_token}", :Accept => "application/xml"})
     data= Hash.from_xml(xml_content)
-#    Rails.logger.info data
+#    Rails.logger.info "Scoreboard.tickets_on_hold: #{data}"
     
     return data["GetTicketsOnHoldResponse"]
+  end
+  
+  #  Returns the number of tickets where PaidDate is today and PaymentStatus is Paid in the specified yard. Also returns the sum of the amounts of those paid tickets.
+  def self.paid_tickets_today_data(auth_token, yard_id)
+    access_token = AccessToken.where(token_string: auth_token).last # Find access token record
+    user = access_token.user # Get access token's user record
+    api_url = "https://#{user.company.dragon_api}/api/yard/#{yard_id}/scoreboard/paidTickets"
+    
+    xml_content = RestClient::Request.execute(method: :get, url: api_url, verify_ssl: false, headers: {:Authorization => "Bearer #{auth_token}", :Accept => "application/xml"})
+    data= Hash.from_xml(xml_content)
+#    Rails.logger.info "Scoreboard.paid_tickets: #{data}"
+    
+    return data["GetPaidTicketsResponse"]
+  end
+  
+  #  Returns a list of commodity totals by type. Since commodity type is a user defined list, we just return them all so that the consumer can decide which to use. 
+  #  Along with the commodity types, it returns the weight (in LB) and dollar amount of each type purchased today. It also returns the average weight and cost over the last 30 days. 
+  #  The divisor for the average division is determined based on the number of records we found, in order to prevent including days where the yard might be closed in the averaging.
+  def self.commodity_totals_by_type(auth_token, yard_id)
+    access_token = AccessToken.where(token_string: auth_token).last # Find access token record
+    user = access_token.user # Get access token's user record
+    api_url = "https://#{user.company.dragon_api}/api/yard/#{yard_id}/scoreboard/commodityTotalsByType"
+    
+    xml_content = RestClient::Request.execute(method: :get, url: api_url, verify_ssl: false, headers: {:Authorization => "Bearer #{auth_token}", :Accept => "application/xml"})
+    data= Hash.from_xml(xml_content)
+#    Rails.logger.info "Scoreboard.commodity_totals_by_type: #{data}"
+    
+    unless data["GetCommodityTotalsResponse"].blank? or data["GetCommodityTotalsResponse"]["CommodityTotals"].blank? or data["GetCommodityTotalsResponse"]["CommodityTotals"]["CommodityTotals"].blank?
+      if data["GetCommodityTotalsResponse"]["CommodityTotals"]["CommodityTotals"].is_a? Hash # Only one result returned, so put it into an array
+        return [data["GetCommodityTotalsResponse"]["CommodityTotals"]["CommodityTotals"]]
+      else # Array of results returned
+        return data["GetCommodityTotalsResponse"]["CommodityTotals"]["CommodityTotals"]
+      end
+    else
+      return []
+    end
+  end
+  
+  # The same call as Scoreboard.commodity_totals_by_type, but  can specify the number of days to go back and look for data.
+  def self.commodity_totals_by_type_number_of_days(auth_token, yard_id, days)
+    access_token = AccessToken.where(token_string: auth_token).last # Find access token record
+    user = access_token.user # Get access token's user record
+    api_url = "https://#{user.company.dragon_api}/api/yard/#{yard_id}/scoreboard/commodityTotalsByType/#{days}"
+    
+    xml_content = RestClient::Request.execute(method: :get, url: api_url, verify_ssl: false, headers: {:Authorization => "Bearer #{auth_token}", :Accept => "application/xml"})
+    data= Hash.from_xml(xml_content)
+#    Rails.logger.info "Scoreboard.commodity_totals_by_type_number_of_days: #{data}"
+    
+    unless data["GetCommodityTotalsResponse"].blank? or data["GetCommodityTotalsResponse"]["CommodityTotals"].blank? or data["GetCommodityTotalsResponse"]["CommodityTotals"]["CommodityTotals"].blank?
+      if data["GetCommodityTotalsResponse"]["CommodityTotals"]["CommodityTotals"].is_a? Hash # Only one result returned, so put it into an array
+        return [data["GetCommodityTotalsResponse"]["CommodityTotals"]["CommodityTotals"]]
+      else # Array of results returned
+        return data["GetCommodityTotalsResponse"]["CommodityTotals"]["CommodityTotals"]
+      end
+    else
+      return []
+    end
+  end
+  
+  # Returns the number of shipments where DateCreated is today and the ShipmentStatus is Held in the specified yard.
+  def self.held_shipments_count(auth_token, yard_id)
+    access_token = AccessToken.where(token_string: auth_token).last # Find access token record
+    user = access_token.user # Get access token's user record
+    api_url = "https://#{user.company.dragon_api}/api/yard/#{yard_id}/scoreboard/heldShipments"
+    
+    xml_content = RestClient::Request.execute(method: :get, url: api_url, verify_ssl: false, headers: {:Authorization => "Bearer #{auth_token}", :Accept => "application/xml"})
+    data= Hash.from_xml(xml_content)
+#    Rails.logger.info "Scoreboard.held_shipments: #{data}"
+    unless data["GetHeldShipmentsResponse"].blank? or data["GetHeldShipmentsResponse"]["HeldShipments"].blank?
+      return data["GetHeldShipmentsResponse"]["HeldShipments"]
+    else
+      return nil
+    end
+  end
+  
+  # Returns the number of shipments where DateOut is today and the ShipmentStatus is Billed or Closed in the specified yard.
+  def self.sent_shipments_count(auth_token, yard_id)
+    access_token = AccessToken.where(token_string: auth_token).last # Find access token record
+    user = access_token.user # Get access token's user record
+    api_url = "https://#{user.company.dragon_api}/api/yard/#{yard_id}/scoreboard/sentShipments"
+    
+    xml_content = RestClient::Request.execute(method: :get, url: api_url, verify_ssl: false, headers: {:Authorization => "Bearer #{auth_token}", :Accept => "application/xml"})
+    data= Hash.from_xml(xml_content)
+#    Rails.logger.info "Scoreboard.sent_shipments: #{data}"
+    unless data["GetSentShipmentsResponse"].blank? or data["GetSentShipmentsResponse"]["SentShipments"].blank?
+      return data["GetSentShipmentsResponse"]["SentShipments"]
+    else
+      return nil
+    end
+  end
+  
+  def self.held_tickets_today(auth_token, yard_id)
+    access_token = AccessToken.where(token_string: auth_token).last # Find access token record
+    user = access_token.user # Get access token's user record
+    api_url = "https://#{user.company.dragon_api}/api/yard/#{yard_id}/tickets/2?d=0&t=100"
+    
+    xml_content = RestClient::Request.execute(method: :get, url: api_url, verify_ssl: false, headers: {:Authorization => "Bearer #{auth_token}", :Accept => "application/xml"})
+    data= Hash.from_xml(xml_content)
+#    Rails.logger.info "Scoreboard.held_tickets_today: #{data}"
+    unless data["ApiPaginatedResponseOfApiTicketHead0UdNujZ0"].blank? or data["ApiPaginatedResponseOfApiTicketHead0UdNujZ0"]["Items"].blank? or data["ApiPaginatedResponseOfApiTicketHead0UdNujZ0"]["Items"]["ApiTicketHead"].blank?
+      if data["ApiPaginatedResponseOfApiTicketHead0UdNujZ0"]["Items"]["ApiTicketHead"].is_a? Hash # Only one result returned, so put it into an array
+        return [data["ApiPaginatedResponseOfApiTicketHead0UdNujZ0"]["Items"]["ApiTicketHead"]]
+      else # Array of results returned
+        return data["ApiPaginatedResponseOfApiTicketHead0UdNujZ0"]["Items"]["ApiTicketHead"]
+      end
+    else
+      return []
+    end
+  end
+  
+  def self.closed_tickets_today(auth_token, yard_id)
+    access_token = AccessToken.where(token_string: auth_token).last # Find access token record
+    user = access_token.user # Get access token's user record
+    api_url = "https://#{user.company.dragon_api}/api/yard/#{yard_id}/tickets/1?d=0&t=100"
+    
+    xml_content = RestClient::Request.execute(method: :get, url: api_url, verify_ssl: false, headers: {:Authorization => "Bearer #{auth_token}", :Accept => "application/xml"})
+    data= Hash.from_xml(xml_content)
+#    Rails.logger.info "Scoreboard.closed_tickets_today: #{data}"
+    unless data["ApiPaginatedResponseOfApiTicketHead0UdNujZ0"].blank? or data["ApiPaginatedResponseOfApiTicketHead0UdNujZ0"]["Items"].blank? or data["ApiPaginatedResponseOfApiTicketHead0UdNujZ0"]["Items"]["ApiTicketHead"].blank?
+      if data["ApiPaginatedResponseOfApiTicketHead0UdNujZ0"]["Items"]["ApiTicketHead"].is_a? Hash # Only one result returned, so put it into an array
+        return [data["ApiPaginatedResponseOfApiTicketHead0UdNujZ0"]["Items"]["ApiTicketHead"]]
+      else # Array of results returned
+        return data["ApiPaginatedResponseOfApiTicketHead0UdNujZ0"]["Items"]["ApiTicketHead"]
+      end
+    else
+      return []
+    end
+  end
+  
+  def self.paid_tickets_today(auth_token, yard_id)
+    access_token = AccessToken.where(token_string: auth_token).last # Find access token record
+    user = access_token.user # Get access token's user record
+    api_url = "https://#{user.company.dragon_api}/api/yard/#{yard_id}/tickets/3?d=0&t=100"
+    
+    xml_content = RestClient::Request.execute(method: :get, url: api_url, verify_ssl: false, headers: {:Authorization => "Bearer #{auth_token}", :Accept => "application/xml"})
+    data= Hash.from_xml(xml_content)
+#    Rails.logger.info "Scoreboard.paid_tickets_today: #{data}"
+    unless data["ApiPaginatedResponseOfApiTicketHead0UdNujZ0"].blank? or data["ApiPaginatedResponseOfApiTicketHead0UdNujZ0"]["Items"].blank? or data["ApiPaginatedResponseOfApiTicketHead0UdNujZ0"]["Items"]["ApiTicketHead"].blank?
+      if data["ApiPaginatedResponseOfApiTicketHead0UdNujZ0"]["Items"]["ApiTicketHead"].is_a? Hash # Only one result returned, so put it into an array
+        return [data["ApiPaginatedResponseOfApiTicketHead0UdNujZ0"]["Items"]["ApiTicketHead"]]
+      else # Array of results returned
+        return data["ApiPaginatedResponseOfApiTicketHead0UdNujZ0"]["Items"]["ApiTicketHead"]
+      end
+    else
+      return []
+    end
+  end
+  
+  def self.closed_shipments_today(auth_token, yard_id) # Non-held shipments
+    access_token = AccessToken.where(token_string: auth_token).last # Find access token record
+    user = access_token.user # Get access token's user record
+    api_url = "https://#{user.company.dragon_api}/api/yard/#{yard_id}/shipping/getshipmentsbycustomer"
+    
+    payload = {
+      "CustomerIds" => [],
+      "StartDate" => "#{Date.today} 00:00:00",
+      "EndDate" => "#{Date.today} 23:59:59"
+      }
+    json_encoded_payload = JSON.generate(payload)
+    Rails.logger.info json_encoded_payload
+    
+    xml_content = RestClient::Request.execute(method: :get, url: api_url, verify_ssl: false, headers: {:Authorization => "Bearer #{auth_token}", :content_type => 'application/json', :Accept => "application/xml"},
+      payload: json_encoded_payload)
+    
+    data= Hash.from_xml(xml_content)
+#    Rails.logger.info "Scoreboard.closed_shipments_today: #{data}"
+    
+    if data["GetShipmentsByCustomerResponse"]["Shipments"]["ShipmentHeadInformation"].blank?# No results, so put into empty array
+      return []
+    else # Array of results returned
+      if data["GetShipmentsByCustomerResponse"]["Shipments"]["ShipmentHeadInformation"].is_a? Hash  # Only one result returned, so put it into an array
+        return [data["GetShipmentsByCustomerResponse"]["Shipments"]["ShipmentHeadInformation"]]
+      else
+        return data["GetShipmentsByCustomerResponse"]["Shipments"]["ShipmentHeadInformation"]
+      end
+    end
   end
   
 end
