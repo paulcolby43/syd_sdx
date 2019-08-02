9549c0a6ff6c3bc9916f409a618d208093a105cd
Jeremy <senn.jeremy@gmail.com>
2019-08-02 07:21:13 -0700

    Unit of measure within ticket line item

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -36,6 +36,10 @@ module ApplicationHelper
     [['Each', 'EA'], ['Pound', 'LB'], ['Net Ton', 'NT'], ['Short Ton', 'ST'], ['Gross Ton', 'GT'], ['Kilogram', 'KG'], ['Hundred Weight', 'CW'], ['Metric Ton', 'MT'], ['Load', 'LD']]
   end
   
+  def units_of_measure_abbreviations
+    ['EA', 'LB', 'NT', 'ST', 'GT', 'KG', 'CW', 'MT', 'LD']
+  end
+  
   def units_of_measure_hash
     Hash[units_of_measure]
   end
