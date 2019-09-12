9cb37586747f6a7bbad48c6f37dab9aa0fead900
Jeremy <senn.jeremy@gmail.com>
2019-09-12 16:43:29 -0700

    Trying to fix database migration issue on production

--- db/migrate/20180921143154_add_active_boolean_to_users.rb
+++ db/migrate/20180921143154_add_active_boolean_to_users.rb
@@ -1,5 +1,8 @@
 class AddActiveBooleanToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :active, :boolean, default: true
+#    add_column :users, :active, :boolean, default: true
+    change_table :tags do |t|
+    # t.boolean :active, default: true
+    end
   end
 end
