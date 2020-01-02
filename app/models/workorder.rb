9eb9f8a72a677a8db0767bcb0373673224e537a3
Jeremy <senn.jeremy@gmail.com>
2020-01-02 06:12:43 -0800

    Catch error if service requests by customer call doesn’t exist in dragon api

--- app/models/workorder.rb
+++ app/models/workorder.rb
@@ -28,17 +28,18 @@ class Workorder
     access_token = AccessToken.where(token_string: auth_token).last # Find access token record
     user = access_token.user # Get access token's user record
     api_url = "https://#{user.company.dragon_api}/api/yard/#{yard_id}/workorder/customer/#{customer_id}/?includeCancelled=false"
-    
-    xml_content = RestClient::Request.execute(method: :get, url: api_url, verify_ssl: false, headers: {:Authorization => "Bearer #{auth_token}", :Accept => "application/xml"})
-    data= Hash.from_xml(xml_content)
-    Rails.logger.info "Workorder.all_by_customer response: #{data}"
-    
-    
-    
-    if data["GetWorkOrdersByCustomerIdResponse"]["Collection"]["CustomerWorkOrderInformation"].is_a? Hash # Only one result returned, so put it into an array
-      return [data["GetWorkOrdersByCustomerIdResponse"]["Collection"]["CustomerWorkOrderInformation"]]
-    else # Array of results returned
-      return data["GetWorkOrdersByCustomerIdResponse"]["Collection"]["CustomerWorkOrderInformation"]
+    begin
+      xml_content = RestClient::Request.execute(method: :get, url: api_url, verify_ssl: false, headers: {:Authorization => "Bearer #{auth_token}", :Accept => "application/xml"})
+      data= Hash.from_xml(xml_content)
+      Rails.logger.info "Workorder.all_by_customer response: #{data}"
+      if data["GetWorkOrdersByCustomerIdResponse"]["Collection"]["CustomerWorkOrderInformation"].is_a? Hash # Only one result returned, so put it into an array
+        return [data["GetWorkOrdersByCustomerIdResponse"]["Collection"]["CustomerWorkOrderInformation"]]
+      else # Array of results returned
+        return data["GetWorkOrdersByCustomerIdResponse"]["Collection"]["CustomerWorkOrderInformation"]
+      end
+    rescue RestClient::ExceptionWithResponse => e
+      Rails.logger.info "Workorder.all_by_customer call: no Dragon API"
+      return nil
     end
   end
   
