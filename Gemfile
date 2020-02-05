c011b5a401f1723899a35e359803b485fd314720
Jeremy <senn.jeremy@gmail.com>
2020-02-05 10:42:39 -0800

    Remove rack-cors gem for now

--- Gemfile
+++ Gemfile
@@ -125,7 +125,7 @@ gem 'actionpack-cloudflare'
 
 
 # https://github.com/cyu/rack-cors
-gem 'rack-cors'
+#gem 'rack-cors'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/application.rb
+++ config/application.rb
@@ -28,12 +28,12 @@ module SydSdx
 #      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
 #    }
     
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-         origins '*'
-         resource '*', :headers => :any, :methods => [:get, :post, :options]
-       end
-    end
+#    config.middleware.insert_before 0, Rack::Cors do
+#      allow do
+#         origins '*'
+#         resource '*', :headers => :any, :methods => [:get, :post, :options]
+#       end
+#    end
     
   end
 end
